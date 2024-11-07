54c36f14f136de1c7dd9ec4f5e0409d29231c746
s-superman909 <secret.superman909@gmail.com>
2024-11-06 23:19:47 -0700

    done

--- 1.py
+++ 1.py
@@ -8,3 +8,10 @@ from time import sleep
 driver = webdriver.Chrome()
 driver.get("https://www.google.com/")
 driver.maximize_window()
+# Search for "selenium"
+search_box = driver.find_element_by_name("q")
+search_box.send_keys("selenium")
+search_box.send_keys(Keys.RETURN)
+sleep(5)
+
+driver.quit()
