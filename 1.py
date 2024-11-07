0e10b9ce95b7ddb7b778b18d0d8de42581945cd8
s-superman909 <secret.superman909@gmail.com>
2024-11-06 23:18:02 -0700

    start

--- 1.py
+++ 1.py
@@ -4,3 +4,7 @@ from selenium.webdriver.common.action_chains import ActionChains
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from time import sleep
+
+driver = webdriver.Chrome()
+driver.get("https://www.google.com/")
+driver.maximize_window()
